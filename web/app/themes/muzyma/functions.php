859e860e0105241c828db7c851cadf693732edf5
Jakob Neumann <jkb.neumann@gmail.com>
2018-12-28 22:29:12 +0100

    rename wp menu icons, update path to icons, add commets for better understanding

--- web/app/themes/muzyma/functions.php
+++ web/app/themes/muzyma/functions.php
@@ -242,13 +242,14 @@ add_action( 'admin_head', 'mp_custom_editor_stylesheet' );
 // SHORTCODES
 // ================================================================================================
 
-// Add Shortcode
-function my_custom_test_shortcode( $atts , $content = null ) {
+// DONT KNOW WHAT FOR THIS SECTION SHOULD BE?!
+        // Add Shortcode
+        // function my_custom_test_shortcode( $atts , $content = null ) {
 
-    return '<strong>' . $content . '</strong>';
+        //     return '<strong>' . $content . '</strong>';
 
-}
-add_shortcode( 'xxtest', 'my_custom_test_shortcode' );
+        // }
+        // add_shortcode( 'xxtest', 'my_custom_test_shortcode' );
 
 
 
@@ -333,100 +334,101 @@ add_filter('image_size_names_choose', 'mp_new_image_sizes');
 // BREADCRUMB NAVIGATION
 // ================================================================================================
 
+// FROM https://blog.kulturbanause.de/2011/08/wordpress-breadcrumb-navigation-ohne-plugin/
 function nav_breadcrumb() {
 
- $delimiter = '&raquo;';
- $home = 'Home';
- $before = '<span class="current-page">';
- $after = '</span>';
+    $delimiter = '&raquo;';
+    $home = 'Home';
+    $before = '<span class="current-page">';
+    $after = '</span>';
 
- if ( !is_home() && !is_front_page() || is_paged() ) {
+    if ( !is_home() && !is_front_page() || is_paged() ) {
 
-     echo '<nav class="breadcrumb container">';
+        echo '<nav class="breadcrumb container">';
 
-     global $post;
-     $homeLink = get_bloginfo('url');
-     echo '<a href="' . $homeLink . '">' . $home . '</a> ' . $delimiter . ' ';
+        global $post;
+        $homeLink = get_bloginfo('url');
+        echo '<a href="' . $homeLink . '">' . $home . '</a> ' . $delimiter . ' ';
 
-     if ( is_category()) {
-         global $wp_query;
-         $cat_obj = $wp_query->get_queried_object();
-         $thisCat = $cat_obj->term_id;
-         $thisCat = get_category($thisCat);
-         $parentCat = get_category($thisCat->parent);
-         if ($thisCat->parent != 0) echo(get_category_parents($parentCat, TRUE, ' ' . $delimiter . ' '));
-         echo $before . single_cat_title('', false) . $after;
+        if ( is_category()) {
+            global $wp_query;
+            $cat_obj = $wp_query->get_queried_object();
+            $thisCat = $cat_obj->term_id;
+            $thisCat = get_category($thisCat);
+            $parentCat = get_category($thisCat->parent);
+            if ($thisCat->parent != 0) echo(get_category_parents($parentCat, TRUE, ' ' . $delimiter . ' '));
+            echo $before . single_cat_title('', false) . $after;
 
-     } elseif ( is_day() ) {
-         echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
-         echo '<a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a> ' . $delimiter . ' ';
-         echo $before . get_the_time('d') . $after;
+        } elseif ( is_day() ) {
+            echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
+            echo '<a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a> ' . $delimiter . ' ';
+            echo $before . get_the_time('d') . $after;
 
-     } elseif ( is_month() ) {
-         echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
-         echo $before . get_the_time('F') . $after;
+        } elseif ( is_month() ) {
+            echo '<a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a> ' . $delimiter . ' ';
+            echo $before . get_the_time('F') . $after;
 
-     } elseif ( is_year() ) {
-         echo $before . get_the_time('Y') . $after;
+        } elseif ( is_year() ) {
+            echo $before . get_the_time('Y') . $after;
 
-     } elseif ( is_single() && !is_attachment() ) {
-         if ( get_post_type() != 'post' ) {
-             $post_type = get_post_type_object(get_post_type());
-             $slug = $post_type->rewrite;
-             echo '<a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a> ' . $delimiter . ' ';
-             echo $before . get_the_title() . $after;
-         } else {
-             $cat = get_the_category(); $cat = $cat[0];
-             echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
-             echo $before . get_the_title() . $after;
-         }
+        } elseif ( is_single() && !is_attachment() ) {
+            if ( get_post_type() != 'post' ) {
+                $post_type = get_post_type_object(get_post_type());
+                $slug = $post_type->rewrite;
+                echo '<a href="' . $homeLink . '/' . $slug['slug'] . '/">' . $post_type->labels->singular_name . '</a> ' . $delimiter . ' ';
+                echo $before . get_the_title() . $after;
+            } else {
+                $cat = get_the_category(); $cat = $cat[0];
+                echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
+                echo $before . get_the_title() . $after;
+            }
 
-     } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
-         $post_type = get_post_type_object(get_post_type());
-         echo $before . $post_type->labels->singular_name . $after;
+        } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
+            $post_type = get_post_type_object(get_post_type());
+            echo $before . $post_type->labels->singular_name . $after;
 
 
-     } elseif ( is_attachment() ) {
-         $parent = get_post($post->post_parent);
-         $cat = get_the_category($parent->ID); $cat = $cat[0];
-         echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
-         echo '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a> ' . $delimiter . ' ';
-         echo $before . get_the_title() . $after;
+        } elseif ( is_attachment() ) {
+            $parent = get_post($post->post_parent);
+            $cat = get_the_category($parent->ID); $cat = $cat[0];
+            echo get_category_parents($cat, TRUE, ' ' . $delimiter . ' ');
+            echo '<a href="' . get_permalink($parent) . '">' . $parent->post_title . '</a> ' . $delimiter . ' ';
+            echo $before . get_the_title() . $after;
 
-     } elseif ( is_page() && !$post->post_parent ) {
-         echo $before . get_the_title() . $after;
+        } elseif ( is_page() && !$post->post_parent ) {
+            echo $before . get_the_title() . $after;
 
-     } elseif ( is_page() && $post->post_parent ) {
-         $parent_id = $post->post_parent;
-         $breadcrumbs = array();
-         while ($parent_id) {
-             $page = get_page($parent_id);
-             $breadcrumbs[] = '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>';
-             $parent_id = $page->post_parent;
-         }
-         $breadcrumbs = array_reverse($breadcrumbs);
-         foreach ($breadcrumbs as $crumb) echo $crumb . ' ' . $delimiter . ' ';
-         echo $before . get_the_title() . $after;
+        } elseif ( is_page() && $post->post_parent ) {
+            $parent_id = $post->post_parent;
+            $breadcrumbs = array();
+            while ($parent_id) {
+                $page = get_page($parent_id);
+                $breadcrumbs[] = '<a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a>';
+                $parent_id = $page->post_parent;
+            }
+            $breadcrumbs = array_reverse($breadcrumbs);
+            foreach ($breadcrumbs as $crumb) echo $crumb . ' ' . $delimiter . ' ';
+            echo $before . get_the_title() . $after;
 
-     } elseif ( is_search() ) {
-         echo $before . 'Ergebnisse für Ihre Suche nach "' . get_search_query() . '"' . $after;
+        } elseif ( is_search() ) {
+            echo $before . 'Ergebnisse für Ihre Suche nach "' . get_search_query() . '"' . $after;
 
-     } elseif ( is_tag() ) {
-         echo $before . 'Beiträge mit dem Schlagwort "' . single_tag_title('', false) . '"' . $after;
+        } elseif ( is_tag() ) {
+            echo $before . 'Beiträge mit dem Schlagwort "' . single_tag_title('', false) . '"' . $after;
 
-     } elseif ( is_404() ) {
-         echo $before . 'Fehler 404' . $after;
-     }
+        } elseif ( is_404() ) {
+            echo $before . 'Fehler 404' . $after;
+        }
 
-     if ( get_query_var('paged') ) {
-         if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
-         echo ': ' . 'Seite' . ' ' . get_query_var('paged');
-         if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
-     }
+        if ( get_query_var('paged') ) {
+            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
+            echo ': ' . 'Seite' . ' ' . get_query_var('paged');
+            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
+        }
 
-     echo '</nav>';
+        echo '</nav>';
 
- }
+    }
 }
 
 
@@ -475,7 +477,7 @@ function mp_cpt_strickmuetzen() {
         'show_ui'               => true,
         'show_in_menu'          => true,
         'menu_position'         => 30,
-        'menu_icon'             => '/app/themes/muzyma/img/knit_cap-icon.png',
+        'menu_icon'             => '/app/themes/muzyma/img/menu-icons/icon-knit_cap.png',
         'show_in_admin_bar'     => true,
         'show_in_nav_menus'     => false,
         'can_export'            => true,
@@ -528,7 +530,7 @@ function mp_cpt_genaehte_muetzen() {
         'show_ui'               => true,
         'show_in_menu'          => true,
         'menu_position'         => 31,
-        'menu_icon'             => '/app/themes/muzyma/img/sew_machine-icon.png',
+        'menu_icon'             => '/app/themes/muzyma/img/menu-icons/icon-sew_machine.png',
         'show_in_admin_bar'     => true,
         'show_in_nav_menus'     => false,
         'can_export'            => true,
@@ -581,7 +583,7 @@ function mp_cpt_haekelmuetzen() {
         'show_ui'               => true,
         'show_in_menu'          => true,
         'menu_position'         => 32,
-        // 'menu_icon'             => '/app/themes/muzyma/img/knit_cap-icon.png',
+        // 'menu_icon'             => '/app/themes/muzyma/img/menu-icons/MISSING_ICON_IMAGE.png',
         'menu_icon'             => 'dashicons-warning',
         'show_in_admin_bar'     => true,
         'show_in_nav_menus'     => false,
@@ -635,7 +637,7 @@ function mp_cpt_yogakissen() {
         'show_ui'               => true,
         'show_in_menu'          => true,
         'menu_position'         => 33,
-        'menu_icon'             => '/app/themes/muzyma/img/pillow-icon.png',
+        'menu_icon'             => '/app/themes/muzyma/img/menu-icons/icon-pillow.png',
         'show_in_admin_bar'     => true,
         'show_in_nav_menus'     => false,
         'can_export'            => true,
