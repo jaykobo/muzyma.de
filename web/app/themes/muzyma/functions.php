c09fc49f21b49e379861490a9a2264eaf1920c84
Jakob Neumann <jkb.neumann@gmail.com>
2018-12-31 17:49:50 +0100

    add custom gallery function, add styling for gallery and wp-caption

--- web/app/themes/muzyma/functions.php
+++ web/app/themes/muzyma/functions.php
@@ -43,6 +43,90 @@ function posts_link_attributes() {
 
 
 
+/*
+ * ADD CUSTOM WP GALLERY OUTPUT
+ * Customize the last half, the output half to suit your projects needs
+ *
+ * From https://stackoverflow.com/a/22660335
+*/
+
+add_filter('post_gallery', 'my_post_gallery', 10, 2);
+function my_post_gallery($output, $attr) {
+    global $post;
+
+    if (isset($attr['orderby'])) {
+        $attr['orderby'] = sanitize_sql_orderby($attr['orderby']);
+        if (!$attr['orderby'])
+        unset($attr['orderby']);
+    }
+
+    extract(shortcode_atts(array(
+        'order' => 'ASC',
+        'orderby' => 'menu_order ID',
+        'id' => $post->ID,
+        'itemtag' => 'dl',
+        'icontag' => 'dt',
+        'captiontag' => 'dd',
+        'columns' => 3,
+        'size' => 'thumbnail',
+        'include' => '',
+        'exclude' => ''
+    ), $attr));
+
+    $id = intval($id);
+    if ('RAND' == $order) $orderby = 'none';
+
+    if (!empty($include)) {
+        $include = preg_replace('/[^0-9,]+/', '', $include);
+        $_attachments = get_posts(array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby));
+
+        $attachments = array();
+        foreach ($_attachments as $key => $val) {
+            $attachments[$val->ID] = $_attachments[$key];
+        }
+    }
+
+    if (empty($attachments)) return '';
+
+    // Here's your actual output, you may customize it to your need
+    $output = "<div class=\"gallery gallery-{$id}\">\n";
+    $output .= "<div class=\"row\">\n";
+
+    // Now you loop through each attachment
+    foreach ($attachments as $id => $attachment) {
+        // Fetch all data related to attachment
+        $img = wp_prepare_attachment_for_js($id);
+
+        // If you want a different size change 'large' to eg. 'medium'
+        $url = $img['sizes']['full']['url']; // Target Image for opening in Lightbox
+        $src = $img['sizes']['single-post']['url'];
+        // $height = $img['sizes']['single-post']['height'];
+        // $width = $img['sizes']['single-post']['width'];
+        $alt = $img['alt'];
+
+        // Store the caption
+        $caption = $img['caption'];
+
+        $output .= "<section class=\"6u\">\n";
+        $output .= "<div class=\"image-wrapper\">\n";
+        $output .= "<a href=\"{$url}\" class=\"image\">\n";
+        $output .= "<img src=\"{$src}\" alt=\"{$alt}\" />\n";
+        $output .= "</a>\n";
+        $output .= "</div>\n";
+
+        // Output the caption if it exists
+        if ($caption) {
+            $output .= "<figcaption class=\"wp-caption-text\">{$caption}</figcaption>\n";
+        }
+        $output .= "</section>\n";
+    }
+
+    $output .= "</div>\n";
+    $output .= "</div>\n";
+
+    return $output;
+}
+
 
 
 
