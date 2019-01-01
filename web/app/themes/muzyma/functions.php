1f6857d36e5198bdc32d7f3354b876dddeb2ed0d
Jakob Neumann <jkb.neumann@gmail.com>
2019-01-01 18:02:42 +0100

    add multiple custom snippets to improve performance and remove unused scripts

--- web/app/themes/muzyma/functions.php
+++ web/app/themes/muzyma/functions.php
@@ -734,7 +734,190 @@ add_action( 'init', 'mp_ct_material', 0 );
 // From https://www.drweb.de/26-nuetzlichsten-funktionellsten-wordpress-snippets/
 // ================================================================================================
 
+/**
+* Disable the emoji's
+*/
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+add_action( 'init', 'disable_emojis' );
+
+/**
+* Filter function used to remove the tinymce emoji plugin.
+*
+* @param array $plugins
+* @return array Difference betwen the two arrays
+*/
+function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+
+/**
+* Remove emoji CDN hostname from DNS prefetching hints.
+*
+* @param array $urls URLs to print for resource hints.
+* @param string $relation_type The relation type the URLs are printed for.
+* @return array Difference betwen the two arrays.
+*/
+function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+    if ( 'dns-prefetch' == $relation_type ) {
+        /** This filter is documented in wp-includes/formatting.php */
+        $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+
+        $urls = array_diff( $urls, array( $emoji_svg_url ) );
+    }
+
+    return $urls;
+}
 
 
+
+/**
+* Dequeue jQuery Migrate Script in WordPress.
+*/
+if ( ! function_exists( 'evolution_remove_jquery_migrate' ) ) :
+
+function evolution_remove_jquery_migrate( &$scripts) {
+    if(!is_admin()) {
+        $scripts->remove( 'jquery');
+        $scripts->add( 'jquery', false, array( 'jquery-core' ), '1.12.4' );
+    }
+}
+add_filter( 'wp_default_scripts', 'evolution_remove_jquery_migrate' );
+endif;
+
+
+
+
+/**
+ * Disable embeds on init.
+ *
+ * - Removes the needed query vars.
+ * - Disables oEmbed discovery.
+ * - Completely removes the related JavaScript.
+ *
+ * @since 1.0.0
+ */
+function evolution_disable_embeds_init() {
+    /* @var WP $wp */
+    global $wp;
+
+    // Remove the embed query var.
+    $wp->public_query_vars = array_diff( $wp->public_query_vars, array(
+        'embed',
+    ) );
+
+    // Remove the REST API endpoint.
+    remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+    // Turn off oEmbed auto discovery.
+    add_filter( 'embed_oembed_discover', '__return_false' );
+
+    // Don't filter oEmbed results.
+    remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+    // Remove oEmbed discovery links.
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+    // Remove oEmbed-specific JavaScript from the front-end and back-end.
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+    add_filter( 'tiny_mce_plugins', 'evolution_disable_embeds_tiny_mce_plugin' );
+
+    // Remove all embeds rewrite rules.
+    add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+
+    // Remove filter of the oEmbed result before any HTTP requests are made.
+    remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+}
+
+add_action( 'init', 'evolution_disable_embeds_init', 9999 );
+
+/**
+ * Removes the 'wpembed' TinyMCE plugin.
+ *
+ * @since 1.0.0
+ *
+ * @param array $plugins List of TinyMCE plugins.
+ * @return array The modified list.
+ */
+function evolution_disable_embeds_tiny_mce_plugin( $plugins ) {
+    return array_diff( $plugins, array( 'wpembed' ) );
+}
+
+/**
+ * Remove all rewrite rules related to embeds.
+ *
+ * @since 1.0.0
+ *
+ * @param array $rules WordPress rewrite rules.
+ * @return array Rewrite rules without embeds rules.
+ */
+function evolution_disable_embeds_rewrites( $rules ) {
+    foreach ( $rules as $rule => $rewrite ) {
+        if ( false !== strpos( $rewrite, 'embed=true' ) ) {
+            unset( $rules[ $rule ] );
+        }
+    }
+
+    return $rules;
+}
+
+/**
+ * Remove embeds rewrite rules on plugin activation.
+ *
+ * @since 1.0.0
+ */
+function evolution_disable_embeds_remove_rewrite_rules() {
+    add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+    flush_rewrite_rules( false );
+}
+
+register_activation_hook( __FILE__, 'evolution_disable_embeds_remove_rewrite_rules' );
+
+/**
+ * Flush rewrite rules on plugin deactivation.
+ *
+ * @since 1.0.0
+ */
+function evolution_disable_embeds_flush_rewrite_rules() {
+    remove_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+    flush_rewrite_rules( false );
+}
+
+register_deactivation_hook( __FILE__, 'evolution_disable_embeds_flush_rewrite_rules' );
+
+
+
+
+/**
+ * Befreit den Header von unnötigen Einträgen
+ */
+add_action('init', 'evolution_remheadlink');
+function evolution_remheadlink()
+{
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wp_generator');
+    remove_action('wp_head', 'index_rel_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'feed_links', 2);
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+    remove_action('wp_head', 'start_post_rel_link', 10, 0);
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+    remove_action('wp_head', 'wp_shortlink_header', 10, 0);
+    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+}
 
 ?>
