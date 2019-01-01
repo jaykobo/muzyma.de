1f482921fa885702ad924f7bc6b272d898983924
Jakob Neumann <jkb.neumann@gmail.com>
2019-01-01 18:26:14 +0100

    update config to limit revisions

--- config/application.php
+++ config/application.php
@@ -81,6 +81,8 @@ Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 Config::define('DISALLOW_FILE_EDIT', true);
 // Disable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', true);
+// Limit revisions
+Config::define('WP_POST_REVISIONS', 5);
 
 /**
  * Debugging Settings
