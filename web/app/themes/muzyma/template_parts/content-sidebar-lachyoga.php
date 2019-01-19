ccd81f96c14e60063d4d2277d52428880db41dd7
Jakob Neumann <jkb.neumann@gmail.com>
2019-01-20 00:06:53 +0100

    edit comment

--- web/app/themes/muzyma/template_parts/content-sidebar-lachyoga.php
+++ web/app/themes/muzyma/template_parts/content-sidebar-lachyoga.php
@@ -9,8 +9,8 @@
                 <img src="<?php the_post_thumbnail_url(); ?>" alt="<?php the_title() ?>" />
             </span>
          <?php }
-         
-        // If post has excerpt then show and add button with link to post - else show content, no link
+
+        // If post has excerpt then show and add button with link to post - else show whole content, no link
         if ( has_excerpt() ) { ?>
             <?php the_excerpt(); ?>
             <p><a href="<?php the_permalink(); ?>" class="button">Weiterlesen »</a></p>
