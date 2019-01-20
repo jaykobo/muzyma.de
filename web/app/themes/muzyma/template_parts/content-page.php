07da766d0864237389937454b52eb4489093fb5e
Jakob Neumann <jkb.neumann@gmail.com>
2019-01-20 01:20:13 +0100

    add alt attribute to image output

--- web/app/themes/muzyma/template_parts/content-page.php
+++ web/app/themes/muzyma/template_parts/content-page.php
@@ -6,8 +6,16 @@
             <p><?php the_field('subtitel'); ?></p>
         <?php endif; ?>
     </header>
+    <?php
+        // IMAGE ALT TEXT: Grab Image Caption OR use Post Title
+        if ( $alt = get_the_post_thumbnail_caption() ) {
+            // Nothing to do here
+        } else {
+            $alt = get_the_title();
+        }
+    ?>
     <?php if( has_post_thumbnail() ): ?>
-        <a href="<?php the_post_thumbnail_url('large'); ?>" class="image featured lightbox">
+        <a href="<?php the_post_thumbnail_url('large'); ?>" class="image featured lightbox" alt="<?php echo $alt?>">
             <?php the_post_thumbnail('single-post'); ?>
         </a>
     <?php endif; ?>
