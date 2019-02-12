0a00ab313897229534fb1a56616d295fedfdf4b0
Jakob Neumann <jkb.neumann@gmail.com>
2019-02-12 23:42:30 +0100

    change single quote

--- web/app/themes/muzyma/single-poesie.php
+++ web/app/themes/muzyma/single-poesie.php
@@ -3,7 +3,7 @@
 <div class="wrapper site-content">
     <div class="container" id="main">
         <mark>Inhalt: single-poesie.php</mark>
-        
+
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
             <?php get_template_part( 'template_parts/content','poesie' ); ?>
         <?php endwhile; else : ?>
@@ -16,7 +16,7 @@
 
 <div class="wrapper dark style1 link-back">
     <div class="container">
-        <a href="<?php echo get_page_link(20); ?>" class="button">&laquo; Zurück zur Übersicht</a>
+        <a href="<?php echo get_page_link(20); ?>" class="button">« Zurück zur Übersicht</a>
     </div>
 </div>
 
