835c962d7bad5b40032ed65ef7a732721ad2384d
Jakob Neumann <jkb.neumann@gmail.com>
2019-02-12 23:42:14 +0100

    add breadcrumb to single product page

--- web/app/themes/muzyma/single-product.php
+++ web/app/themes/muzyma/single-product.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
 
+<?php
+    // Getting the post type of the current post
+    $current_post_type_object = get_post_type_object(get_post_type($post->ID));
+    $current_post_type_name   = $current_post_type_object->label;
+    $current_post_type        = get_post_type();
+    $strip_slug_to_url        = str_replace('p-', '', $current_post_type);
+    $parent_page_url          = home_url('/').'handgemacht-'.$strip_slug_to_url;
+?>
+
+<div class="wrapper breadcrumb">
+    <div class="container">
+        <nav class="breadcrumb">
+        « <a href="<?php echo $parent_page_url; ?>"><?php echo $current_post_type_name; ?></a>
+        </nav>
+    </div>
+</div>
+
 <div class="wrapper site-content">
     <div class="container" id="main">
         <mark>Inhalt: single-product.php</mark>
@@ -13,18 +30,9 @@
     </div>
 </div>
 
-
-<?php
-    // Getting the post type of the current post
-    $current_post_type_object = get_post_type_object(get_post_type($post->ID));
-    $current_post_type_name   = get_post_type();
-    $strip_slug_to_url        = str_replace('p-', '', $current_post_type_name);
-    $parent_page_url          = home_url('/').'handgemacht-'.$strip_slug_to_url;
-?>
-
 <div class="wrapper dark style1 link-back">
     <div class="container">
-        <a href="<?php echo $parent_page_url; ?>" class="button">&laquo; Zurück zur Übersicht</a>
+        <a href="<?php echo $parent_page_url; ?>" class="button">« Zurück zur Übersicht</a>
     </div>
 </div>
 
