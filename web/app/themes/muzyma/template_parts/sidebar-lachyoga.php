4a0451e391873634441762b789aaebb205c47903
Jakob Neumann <jkb.neumann@gmail.com>
2019-02-13 01:18:31 +0100

    add link to category page, add styling

--- web/app/themes/muzyma/template_parts/sidebar-lachyoga.php
+++ web/app/themes/muzyma/template_parts/sidebar-lachyoga.php
@@ -1,9 +1,20 @@
 <section id="sidebar">
 
     <?php
+        $category_name = 'Lachyoga';
+        // Get Category ID by Name
+        $category_id = get_cat_ID( $category_name );
+
+        // Get Category Object by ID
+        $category = get_category( $category_id );
+
+        // Gather other information based on Category Object
+        $category_slug = $category->slug;
+        $category_url  = get_category_link( $category_id );
+
 
         $args = array(
-            'category_name'  => 'lachyoga-news',
+            'category_name'  => $category_slug,
             'post_type'      => 'any',
             'order'          => 'ASC',
             'orderby'        => 'date',
@@ -17,6 +28,10 @@
     <?php endwhile; else : ?>
         <?php get_template_part( 'template_parts/content','error' ); ?>
     <?php endif; wp_reset_postdata(); ?>
+
+    <section class="sidebar-category-link">
+        <a href="<?php echo $category_url; ?>" class="button">Alle Beiträge zu <?php echo $category_name; ?>&nbsp;»</a>
+    </section>
 
 
 </section>
