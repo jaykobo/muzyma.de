2cf20e9b318f24294348554b81a1737125df164b
Jakob Neumann <jkb.neumann@gmail.com>
2019-11-29 23:48:01 +0100

    update meta viewport informations

--- web/app/themes/muzyma/header.php
+++ web/app/themes/muzyma/header.php
@@ -2,7 +2,6 @@
 <html lang="<?php bloginfo( 'language' ); ?>">
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
-        <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
 
         <!--[if lte IE 8]><script src="<?php echo esc_url( get_template_directory_uri() ) ?>/css/ie/html5shiv.js"></script><![endif]-->
 
